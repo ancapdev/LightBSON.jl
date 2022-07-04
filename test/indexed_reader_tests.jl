897663c10e8a977642b891a2e6c489cf66273152
Dan Taras <dan.taras@graystonetrading.com>
2022-07-04 14:20:22 +0200

    Support for arrays in IndexedBSONReader

--- test/indexed_reader_tests.jl
+++ test/indexed_reader_tests.jl
@@ -5,9 +5,15 @@
     writer["y"] = w -> begin
         w["x"] = Int64(2)
     end
+    v = [1, 2, 3]
+    writer["v"] = v
     close(writer)
     reader = BSONReader(buf)
     ireader = IndexedBSONReader(BSONIndex(128), reader)
     @test ireader["x"][Int64] == 1
     @test ireader["y"]["x"][Int64] == 2
+    @test ireader["v"][Vector{Int64}] == [1, 2, 3]
+    for (i, x) in enumerate(v)
+        @test ireader["v"][i][Int64] == x
+    end
 end
